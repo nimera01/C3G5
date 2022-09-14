565c47158af343ffc5a2e244c6cdbc2400753375
NMR <nimera2000@gmail.com>
2022-09-14 08:15:58 -0500

    agregando clase

---
+++ src/githubg5c3/Cargo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package githubg5c3;
+
+/**
+ *
+ * @author nimer
+ */
+public class Cargo {
+    
+    public static void main(String[] args){
+    System.out.println("Cargo");
+    }
+}
